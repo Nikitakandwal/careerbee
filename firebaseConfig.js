d1427027bd9057c0c9e3d5d75d9385702ebddb5d
Nikita <kandwalnikita70@gmail.com>
2024-05-18 12:39:52 +0530

    removed secret keyS

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth} from "firebase/auth";
  
 const firebaseConfig = {
-  apiKey: "AIzaSyBKT3BBhx22R2WLr3BRfvdMHy1-9_fUsKg",
-  authDomain: "expo-login-543b1.firebaseapp.com",
-  projectId: "expo-login-543b1",
-  storageBucket: "expo-login-543b1.appspot.com",
-  messagingSenderId: "461986594919",
-  appId: "1:461986594919:web:ead8f127a3ed37fe2e94c2"
+  apiKey: "apikey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-const provider = new GoogleAuthProvider();
-
-export { firebaseConfig, auth, provider }; // Exporting auth and provider
-
-//android: 75448086380-g107gh1og0v7heo01rc3rf0aau5d2rpu.apps.googleusercontent.com
+export { firebaseConfig, auth };  
